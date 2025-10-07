84c167225f6502c1d1fdb36d5807651f79b3971e
Jay Two <jaytwo@jaytwo.kr>
2025-10-08 04:28:11 +0900

    fix version.hpp

--- j2_library/include/j2_library/j2_library.hpp
+++ j2_library/include/j2_library/j2_library.hpp
@@ -5,7 +5,8 @@
 #include <vector>
 
 #include "j2_library/export.hpp" // For J2LIB_API macro definition
-#include "j2_library/version.hpp" // For version information
+// #include "j2_library/version.hpp" // For version information
+#include "version.hpp" // For version information
 #include "j2_library/core/single_function.hpp" // For Single Function definitions
 #include "j2_library/core/mutex_string.hpp" // For mutex_string class definition
 
