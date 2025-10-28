29a4e7b8b18fe7a7234e91521e4916baf1f7a033
Jay Two <j2doll@gmail.com>
2025-10-28 23:20:02 +0900

    test

--- hello-log-manager/main.cpp
+++ hello-log-manager/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <filesystem>
 
 #include <spdlog/spdlog.h>
 
@@ -22,22 +23,24 @@
 
 int main() {
     // 기본 설정 파일명을 j2_logger_manager_config.ini 로 변경
+   
+    std::string cmakePath = CMAKE_SOURCE_DIR_PATH; // CMake에서 전달된 매크로 → 문자열
+    std::filesystem::path basePath(cmakePath); // filesystem::path 로 감싸기
+    std::filesystem::path filePath = basePath / "j2_logger_manager_config_korean.ini"; // 연산자 / 로 하위 경로 추가
+    std::string filePathStr = filePath.string(); // 경로를 문자열로 변환
 
-    // std::string defaultConfig = "j2_logger_manager_config.ini";
-    std::string defaultConfig = "C:\\workspace\\dev\\github\\LoggerManager\\j2_logger_manager_config_korean.ini";
-    // std::string defaultConfig = "C:\\not_exist.ini";
-
-    std::string sectionName = "Log";
-    std::string loggerName = hname;
+    std::string sectionName = "Log"; // INI 파일의 섹션 이름
+    std::string loggerName = hname; // spdlog 에서 사용할 로거 이름
 
     std::string envName = ""; // 환경 변수로 INI 경로 설정하지 않으면 "" 사용
-    // std::string envName       = "LOG_MANAGER_CONFIG_PATH";
+    // std::string envName       = "LOG_MANAGER_CONFIG_PATH"; // LOG_MANAGER_CONFIG_PATH 환경변수에 값이 있는 경우, 해당 값을 INI 파일 경로로 간주하여 파일을 연다. 
 
     j2::log::logger_manager logMgr;
-    if (!logMgr.init(defaultConfig, sectionName, loggerName, envName)) {
-        return 1;
+    if (!logMgr.init(filePathStr, sectionName, loggerName, envName)) {
+        return 1; // 초기화 실패 시 종료
     }
 
+    // 로그 레벨 별 메시지를 계속 출력
     while (true) {
         ht("trace message");
         hd("debug message");
