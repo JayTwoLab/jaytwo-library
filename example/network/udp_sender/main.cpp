a2be570053dd27e714d05b56dc194652d96b1947
Jay Two <j2doll@gmail.com>
2025-12-09 22:38:16 +0900

    test

--- example/network/udp_sender/main.cpp
+++ example/network/udp_sender/main.cpp
@@ -7,17 +7,25 @@
 
 #include "j2_library/j2_library.hpp"
 
+std::string get_current_time_string();
+
 void udp_sender_thread(j2::network::udp::udp_sender& sender, std::atomic<bool>& running_flag) {
-    int counter = 0;
     while (running_flag.load()) { // Loop until the running_flag is set to false
-        sender.send_data_to("Hello, UDP Server! Message " + std::to_string(counter), "127.0.0.1", 12345);
-        std::cout << " send data..." << std::endl;
-        ++counter;
-        std::this_thread::sleep_for(std::chrono::seconds(3));
+
+        // NOTE: send_data_to does not use setting of server ip and port. only use parameters.
+        sender.send_data_to("Hello Unicast", "127.0.0.1", 12346); // Send unicast
+        std::cout << "unicast sent at 127.0.0.1:12346" << std::endl;
+
+        sender.set_multicast_ttl(2); // Multicast Egress TTL Settings (Optional)
+        sender.send_data_to("Hello Multicast", "239.255.255.250", 12347); // Send multicast 
+        std::cout << "multicast sent at 239.255.255.250:12347" << std::endl;
+
+        sender.send_data_to("Hello Broadcast", "255.255.255.255", 12349); // Broadcast transmission
+        std::cout << "broadcast sent at 255.255.255.255:12349" << std::endl;
+
+        std::this_thread::sleep_for(std::chrono::seconds(5));
     }
 }
-
-std::string get_current_time_string();
 
 int main() {
 #ifdef _WIN32
@@ -29,18 +37,13 @@ int main() {
 #endif
 
     j2::network::udp::udp_sender sender;
-    // sender.setServer("127.0.0.1", 12345);
+
+    sender.setServer("127.0.0.1", 12345);
 
     if (!sender.create()) {
+        std::cerr << "Failed to create UDP sender" << std::endl;
         return 1;
     }
-
-    sender.send_data_to("Hello Unicast", "127.0.0.1", 12345); // Send unicast
-
-    sender.set_multicast_ttl(2); // Multicast Egress TTL Settings (Optional)
-    sender.send_data_to("Hello Multicast", "239.255.255.250", 12347); // Send multicast 
-
-    sender.send_data_to("Hello Broadcast", "255.255.255.255", 12345); // Broadcast transmission
 
     std::atomic<bool> running_flag(true);
     std::thread sender_thread(udp_sender_thread, std::ref(sender), std::ref(running_flag));
@@ -48,7 +51,7 @@ int main() {
     // Main thread loop with 1-second sleep
     while (true) {
         std::this_thread::sleep_for(std::chrono::seconds(1)); // Wait a bit
-        std::cout << "Main thread is running..." << std::endl;
+        // std::cout << "Main thread is running..." << std::endl;
 
         // It can be set to stop sending when certain conditions are met.
         // example> if (condition) { break; }
@@ -70,13 +73,10 @@ int main() {
 
 std::string get_current_time_string()
 {
-    // 현재 시간 포인트 획득
     auto now = std::chrono::system_clock::now();
 
-    // time_t 로 변환
     std::time_t now_time_t = std::chrono::system_clock::to_time_t(now);
 
-    // struct tm 로 변환 (로컬 시간)
     std::tm local_tm{};
 #ifdef _WIN32
     localtime_s(&local_tm, &now_time_t);  // Windows
@@ -84,9 +84,8 @@ std::string get_current_time_string()
     localtime_r(&now_time_t, &local_tm);  // Linux
 #endif
 
-    // 문자열로 포맷팅
     std::ostringstream oss;
-    oss << std::put_time(&local_tm, "%Y-%m-%d %H:%M:%S");  // 원하는 형식 지정
+    oss << std::put_time(&local_tm, "%Y-%m-%d %H:%M:%S");  
 
     return oss.str();
 }
