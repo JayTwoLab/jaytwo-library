3b1fcb4f4bb46533ec69848036ff3dff4b632dc6
Jay Two <j2doll@gmail.com>
2025-12-28 09:26:19 +0900

    test

--- example/use_j2_installed/src/main.cpp
+++ example/use_j2_installed/src/main.cpp
@@ -4,5 +4,12 @@
 int main()
 {
     std::cout << "j2 library version: " << j2::VersionString << std::endl;
+
+    j2::bit::bit_array bits1; // J2LIB_API class usage
+    bits1.set(0);
+    bits1.set(2);
+    bits1.set(4);
+    std::cout << "bits1: " << bits1.to_string() << std::endl;
+
     return 0;
 }
