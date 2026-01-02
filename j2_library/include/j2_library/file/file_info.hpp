86533e8c715139fe80f12a09c8633e93338555ed
Jay Two <j2doll@gmail.com>
2026-01-02 16:18:30 +0900

    testing

---
+++ j2_library/include/j2_library/file/file_info.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+#include <filesystem>
+
+#include "j2_library/export.hpp"
+
+namespace j2::file {
+
+    class J2LIB_API file_info {
+    public:
+        explicit file_info(const std::string& path);
+
+        bool exists() const;
+        bool is_file() const;
+        bool is_dir() const;
+
+        std::string file_name() const;
+        std::string extension() const;
+        std::string parent_path() const;
+        std::string absolute_path() const;
+
+        std::uintmax_t file_size() const;
+
+    private:
+        std::filesystem::path path_;
+    };
+
+} // namespace j2::file
